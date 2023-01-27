c950476b50aba0df7fe227c6c1dff5f6ff62f1f6
shilp-ds <shilpi.sharma@dotsquares.com>
2023-01-28 00:01:27 +0530

    changes

--- src/components/locatorPage/LocationCard.tsx
+++ src/components/locatorPage/LocationCard.tsx
@@ -4,19 +4,22 @@ import { CardComponent } from "@yext/search-ui-react";
 import { Location } from "..//../types/search/locations";
 import Hours from '..//../components/commons/hours';
 import Address from "..//../components/commons/Address";
-import phonePin from "..//../images/phone.svg";
+import addressicon from "../../images/marker.svg";
+import phone from "../../images/phone.svg";
+import watch from "../../images/watch.svg";
 import { formatPhoneNumber, formatPhoneNumberIntl } from 'react-phone-number-input';
 import OpenCloseStatus from "..//../components/commons/OpenCloseStatus";
 import Today from "../Today";
 import GetDirection from "../GetDirection";
+import UseMyLocation from "./UseMyLocation";
 const metersToMiles = (meters: number) => {
   const miles = meters * 0.000621371;
   return miles.toFixed(2);
-}
+};
 
 const LocationCard: CardComponent<Location> = ({ result }) => {
   console.log(result);
-  const { address, hours, mainPhone, timezone } = result.rawData;
+  const { address, hours, mainPhone, timezone, additionalHoursText, } = result.rawData;
   const formattedPhone = formatPhoneNumber(mainPhone);
 
   const [timeStatus, setTimeStatus] = useState("");
@@ -30,17 +33,89 @@ const LocationCard: CardComponent<Location> = ({ result }) => {
 
   return (
     <div className={`location result`} id={`result-${result.index}`}>
-     
+     <UseMyLocation/>
+     <div className="relative w-full">
       <h3 className=""><a href={result.rawData.slug}>{result.rawData.name} </a>
       </h3>
-      {metersToMiles(result.distance)} miles
+      <div className="miles">
+          <span className="icon">
+            <img src={addressicon} alt="" />
+          </span>{" "}
+          {metersToMiles(result.distance ?? 0)} mi
+        </div>
+      </div>
 
       {/* <p className="text-sm text-slate-700">{address.line1}</p>
       <p className="text-sm text-slate-700">{address.city}, {address.region}, {address.postalCode} </p> */}
       <Address address={address} />
-      <h4><a href={`tel:${result.rawData.mainPhone}`}>{result.rawData.mainPhone}</a></h4>
-     
-      <Today data={result.rawData.hours} />
+      <div className="icon-row ">
+            {" "}
+            <span className="icon">
+              <img src={phone} />
+            </span>
+      <h4><a href={`tel:${result.rawData.mainPhone}`}>{result.rawData.mainPhone}</a></h4></div>
+      <div className="open-close notHighlight">
+          <div className="hours-sec ">
+            <div className="OpenCloseStatus notHighlight ">
+              {result.rawData.hours ? (
+                <>
+                  {Object.keys(result.rawData.hours).length > 1 ? (
+                    <>
+                      <div className="hours-labels icon-row">
+                        <span className="icon">
+                        <img src={watch}></img>
+                        </span>
+                        <a
+                          className={timeStatus}
+                          href="javascript:void(0);"
+                          onClick={onOpenHide}
+                        >
+                          <OpenCloseStatus
+                            timezone={timezone}
+                            hours={hours}
+                          ></OpenCloseStatus>
+                        </a>
+                        <svg
+                          onClick={onOpenHide}
+                          icon-row
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="9.585"
+                          height="4.793"
+                          viewBox="0 0 9.585 4.793"
+                        >
+                          <path
+                            id="hrd-drop"
+                            d="M9,13.5l4.793,4.793L18.585,13.5Z"
+                            transform="translate(-9 -13.5)"
+                            fill="#00363f"
+                          ></path>
+                        </svg>
+                      </div>
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </>
+              ) : (
+                <></>
+              )}
+              {hours ? (
+                <>
+                  <div className={timeStatus + " daylist"}>
+                    <Hours
+                      key={result.rawData.id}
+                      hours={hours}
+                      additionalHoursText={additionalHoursText}
+                    />
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>{" "}
+        </div>
+      
       <GetDirection buttonText="Get Direction" address={address} latitude={result.rawData?.cityCoordinate?.latitude} longitude={result.rawData?.cityCoordinate?.longitude} label="Get direction"/>
     </div >
   );
